0298fb572fb539671714e8efaedcb29d2f2436db
SC <diatoms_flight.4h@icloud.com>
2025-04-05 12:18:50 +0200

    Initial Commit

---
+++ Vibe ID/Vibe_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibe_IDApp.swift
+//  Vibe ID
+//
+//  Created by Carlos on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Vibe_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
